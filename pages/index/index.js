0aec18bac7a70a2d0f49573a7176ea7910336179
Alex Smith <smithavt14@gmail.com>
2020-12-23 17:30:06 +0800

    initialized repository

---
+++ pages/index/index.js
@@ -0,0 +1,9 @@
+Page({
+  data: {
+    images: ['/images/1.jpeg', '/images/2.jpeg', '/images/3.jpeg', '/images/4.jpeg', '/images/5.jpeg'],
+    snow: new Array(180),
+    title: 'Merry Christmas 🎄',
+    subtitle: 'from Alex, Nancy & Heizi',
+    note: 'Wishing you and yours a happy holidays'
+  }
+})
